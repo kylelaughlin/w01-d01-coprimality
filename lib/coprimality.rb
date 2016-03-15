4f39ceab3e8097950ef012c996e0f5dafdfd2e3d
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-14 20:30:40 -0500

    Find factors of a number

--- lib/coprimality.rb
+++ lib/coprimality.rb
@@ -1,6 +1,17 @@
 # This method takes two integers, m and n, and determines whether those integers are co-prime
-
-def determine_coprimality(m, n)
-  #write your method here
-  return true
-end
+require 'pry'
+#def determine_coprimality(m, n)
+  def prime_factors (number)
+    factors_array = Array.new
+    i=1
+    while i <= number / 2
+      if number % i == 0
+        factors_array << i
+      end
+      i += 1
+    end
+    factors_array
+  end
+  puts prime_factors(15)
+  binding.pry
+#end
